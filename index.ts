0d660ddd6d4abb824fc991618d1dc08407965a28
nemanja.karaklajic <nemanjakaraklajic@teletrader.com>
2025-04-10 14:50:09 +0200

    Update map to add dynamic markers on click
    Replaced the static marker with a click event listener that adds dynamic markers to the map based on user interaction. Each marker displays a custom popup with specific styling and content when clicked.

--- index.ts
+++ index.ts
@@ -23,9 +23,23 @@ const getCoords = function () {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker(coords).addTo(map)
-            .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-            .openPopup();
+
+        map.on("click", function (mapEvent) {
+            console.log(mapEvent)
+
+            const {lat, lng} = mapEvent.latlng
+
+            L.marker([lat, lng]).addTo(map)
+                .bindPopup(L.popup({
+                    maxWidth: 300,
+                    minWidth: 100,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: `running-popup`,
+                }))
+                .setPopupContent(`Running`)
+                .openPopup();
+        });
 
         console.log({latitude, longitude})
         console.log(`https://www.google.rs/maps/@${latitude},${longitude}`)
