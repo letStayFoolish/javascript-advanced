8ec36de0a7a2b2ade7bf5e1ae4f96608db8fe063
nemanja.karaklajic <nemanjakaraklajic@teletrader.com>
2025-04-11 12:42:28 +0200

    Refactor type assertions and enhance map interaction handling
    Added explicit type assertions for form and input elements to improve type safety. Refactored map event handling to initialize input fields and display form upon map clicks. Improved form behavior with dynamic toggling of input fields based on workout type.

--- index.ts
+++ index.ts
@@ -2,13 +2,15 @@ console.log("Hello world!");
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
+const form = document.querySelector('.form') as HTMLFormElement;
 const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const inputType = document.querySelector('.form__input--type') as HTMLInputElement;
+const inputDistance = document.querySelector('.form__input--distance') as HTMLInputElement;
+const inputDuration = document.querySelector('.form__input--duration') as HTMLInputElement;
+const inputCadence = document.querySelector('.form__input--cadence') as HTMLInputElement;
+const inputElevation = document.querySelector('.form__input--elevation') as HTMLInputElement;
+
+let map: any, mapEvent: any;
 
 const getCoords = function () {
     return navigator.geolocation.getCurrentPosition(position => {
@@ -17,28 +19,37 @@ const getCoords = function () {
 
         const coords = [latitude, longitude]
 
-        const map = L.map('map').setView(coords, 13); // 'map' id name of element where we gonna store map <div id="map"></div>
+        map = L.map('map').setView(coords, 13); // 'map' id name of element where we gonna store map <div id="map"></div>
 
         L.tileLayer(`https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png`, {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
 
-        map.on("click", function (mapEvent) {
-            console.log(mapEvent)
+        map.on("click", function (event: any) {
+            // Initially clear the form input fields
+            inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
 
-            const {lat, lng} = mapEvent.latlng
 
-            L.marker([lat, lng]).addTo(map)
-                .bindPopup(L.popup({
-                    maxWidth: 300,
-                    minWidth: 100,
-                    autoClose: false,
-                    closeOnClick: false,
-                    className: `running-popup`,
-                }))
-                .setPopupContent(`Running`)
-                .openPopup();
+            form.classList.remove('hidden');
+            inputDistance.focus();
+
+            mapEvent = event;
+
+            // console.log(mapEvent)
+            //
+            // const {lat, lng} = mapEvent.latlng
+            //
+            // L.marker([lat, lng]).addTo(map)
+            //     .bindPopup(L.popup({
+            //         maxWidth: 300,
+            //         minWidth: 100,
+            //         autoClose: false,
+            //         closeOnClick: false,
+            //         className: `running-popup`,
+            //     }))
+            //     .setPopupContent(`Running`)
+            //     .openPopup();
         });
 
         console.log({latitude, longitude})
@@ -49,4 +60,26 @@ const getCoords = function () {
 }
 
 getCoords();
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const {lat, lng} = mapEvent.latlng
+
+    L.marker([lat, lng]).addTo(map)
+        .bindPopup(L.popup({
+            maxWidth: 300,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: `running-popup`,
+        }))
+        .setPopupContent(`Running`)
+        .openPopup();
+})
+
+inputType.addEventListener('change', (e) => {
+    (inputCadence.closest(".form__row") as HTMLDivElement).classList.toggle("form__row--hidden");
+    (inputElevation.closest(".form__row") as HTMLDivElement).classList.toggle("form__row--hidden");
+})
 
